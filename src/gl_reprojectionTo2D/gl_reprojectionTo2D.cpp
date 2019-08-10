940a3a7d738e0091a4567fcbeaa164b0659a6ec2
Simon <grac@photoneo.com>
2019-08-10 22:24:43 +0200

    2D reprojection with depthmap

--- src/gl_reprojectionTo2D/gl_reprojectionTo2D.cpp
+++ src/gl_reprojectionTo2D/gl_reprojectionTo2D.cpp
@@ -1,12 +1,8 @@
-// Example gl_diffuse
-// - Demonstration of simple diffuse lighting with one directional light source
-
 #include <glm/glm.hpp>
 #include <glm/gtc/random.hpp>
-#include <unistd.h>
-
 
 #include <ppgso/ppgso.h>
+#include <unistd.h>
 
 #include <shaders/diffuse_vert_glsl.h>
 #include <shaders/diffuse_frag_glsl.h>
@@ -18,9 +14,9 @@ using namespace ppgso;
 
 const unsigned int SIZE = 800;
 
-
+// desired rotations and num. of rotation steps
 vector<vector<int>> rotations;
-int steps = 10;
+int steps = 8;
 vector<int> view;
 
 /*!
@@ -30,15 +26,12 @@ class DiffuseWindow : public Window {
 private:
     Shader program = {diffuse_vert_glsl, diffuse_frag_glsl};
     Shader programDepth = {diffuse_vert_glsl, depth_frag_glsl};
+
     Texture texture = {image::loadBMP("duck.bmp")};
     Mesh object = {"duck_scene_blender_triangulate.obj"};
 
-    int rotationX = 0;
-    int rotationY = 0;
-    int rotationZ = 0;
-
+    std::string snapshotName;
     int spashotID = 0;
-
 
 public:
     /*!
@@ -48,7 +41,6 @@ public:
         // Set camera position with perspective projection
         program.setUniform("ProjectionMatrix", perspective((PI / 180.f) * 60.0f, 1.0f, 0.1f, 10.0f));
         programDepth.setUniform("ProjectionMatrix", perspective((PI / 180.f) * 60.0f, 1.0f, 0.1f, 10.0f));
-
 
         // Set the light direction, assumes simple white directional light
         program.setUniform("LightDirection", normalize(vec3{-1.0f, -1.0f, -1.0f}));
@@ -68,37 +60,12 @@ public:
         glCullFace(GL_BACK);
     }
 
-    /*!
-     * Handles pressed key when the window is focused
-     * @param key Key code of the key being pressed/released
-     * @param scanCode Scan code of the key being pressed/released
-     * @param action Action indicating the key state change
-     * @param mods Additional modifiers to consider
-     */
+
     void onKey(int key, int scanCode, int action, int mods) override {
         if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
             // Set new random light direction
             program.setUniform("LightDirection", (vec3)sphericalRand(1.0));
 
-        }
-
-        if (action == GLFW_PRESS || action == GLFW_REPEAT) {
-            switch (key) {
-                case GLFW_KEY_DOWN:
-                    rotationX = (rotationX - 1) % 360;
-                    break;
-                case GLFW_KEY_UP:
-                    rotationX = (rotationX + 1) % 360;
-                    break;
-                case GLFW_KEY_LEFT:
-                    rotationY = (rotationY - 1) % 360;
-                    break;
-                case GLFW_KEY_RIGHT:
-                    rotationY = (rotationY + 1) % 360;
-                    break;
-                default:
-                    break;
-            }
         }
     }
 
@@ -107,7 +74,7 @@ public:
      */
     void onIdle() override {
         // Set gray background
-        glClearColor(.5f,.5f,.5f,0);
+        glClearColor(1.0f, 1.0f, 1.0f, 0);
 
         // Clear depth and color buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -115,38 +82,38 @@ public:
         // Get time
         auto time = glfwGetTime();
 
-        // Create object matrix that rotates in time
-        //auto sphereMat = mat4{1.0f};
-
-        // Set camera position/rotation - for example, translate camera a bit backwards (positive value in Z axis), so we can see the objects
+        // Rotate camera according to view
         auto cameraMat = translate(mat4{1.0f}, {0.0f, 0.0f, -0.2f});
         cameraMat = rotate(cameraMat, (float(view[0]) / 360.0f) * 2.0f * 3.14f, {1.0f, 0.0f, 0.0f});
         cameraMat = rotate(cameraMat, (float(view[1]) / 360.0f) * 2.0f * 3.14f, {0.0f, 1.0f, 0.0f});
         cameraMat = rotate(cameraMat, (float(view[2]) / 360.0f) * 2.0f * 3.14f, {0.0f, 0.0f, 1.0f});
 
-
         program.setUniform("ViewMatrix", cameraMat);
         programDepth.setUniform("ViewMatrix", cameraMat);
 
-        // Set the matrix as model matrix for current program
+
 
         // Create object matrix
         auto sphereMat = mat4{1.0f};
-        //sphereMat = rotate(sphereMat, (float(view[0]) / 360.0f) * 2.0f * 3.14f, {1.0f, 0.0f, 0.0f});
-        //sphereMat = rotate(sphereMat, (float(view[1]) / 360.0f) * 2.0f * 3.14f, {0.0f, 1.0f, 0.0f});
-        //sphereMat = rotate(sphereMat, (float(view[2]) / 360.0f) * 2.0f * 3.14f, {0.0f, 0.0f, 1.0f});
+//        sphereMat = rotate(sphereMat, (float(view[0]) / 360.0f) * 2.0f * 3.14f, {1.0f, 0.0f, 0.0f});
+//        sphereMat = rotate(sphereMat, (float(view[1]) / 360.0f) * 2.0f * 3.14f, {0.0f, 1.0f, 0.0f});
+//        sphereMat = rotate(sphereMat, (float(view[2]) / 360.0f) * 2.0f * 3.14f, {0.0f, 0.0f, 1.0f});
 
-        program.setUniform("ModelMatrix", sphereMat);
+        // Render object view
         spashotID++;
-        std::string snapshotName = "depthMaps/depth_snap" + to_string(spashotID);;
-        // Render object
-        object.renderAndMakeSnapshots(SIZE, SIZE, snapshotName);
 
+        // Set the matrix as model matrix for current program
         programDepth.setUniform("ModelMatrix", sphereMat);
+
+        // Render object depth map
+        snapshotName = "depthMaps/depth_snap" + to_string(spashotID);;
+        object.render();
+
+        // Set the matrix as model matrix for current program
+        program.setUniform("ModelMatrix", sphereMat);
+
         snapshotName = "snapshots/snap" + to_string(spashotID);
-
-        object.renderAndMakeSnapshots(SIZE, SIZE, snapshotName);
-
+        object.render();
     }
 };
 
@@ -164,7 +131,7 @@ void generateRotationCombinations() {
 }
 
 int main() {
-// Create a window with OpenGL 3.3 enabled
+    // Create a window with OpenGL 3.3 enabled
     DiffuseWindow window;
 
     // Generate rotation combinations matrix
